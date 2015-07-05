71d5ae4ff7306464e6d589f14317d3269666c764
HyimHK <homingyim@gmail.com>
2015-07-05 11:44:21 -0700

    s

---
+++ js/3D4E.js
@@ -0,0 +1,22 @@
+function initialize() {
+    $(function () {
+        $("a.page-scroll").bind("click", function (e) {
+            var t = $(this);
+            $("html, body").stop().animate({scrollTop: $(t.attr("href")).offset().top - $(".navbar").height()}, 750, "easeInOutExpo");
+            e.preventDefault();
+        });
+    }
+    );
+    
+    $("#dummy").attr("min-height", $(".navbar").height());
+    
+    var w = $(window).width() * .75;
+    $('#my-slider').sliderPro({
+        arrows: true,
+        waitForLayers: true,
+        width: w,
+        autoHeight: true,
+        fade: true,
+        buttons: false,
+    });
+}
