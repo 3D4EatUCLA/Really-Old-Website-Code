0e2431bc5e26215fe2d7f015a6c902f076fdfba4
HyimHK <homingyim@gmail.com>
2015-07-10 23:03:57 -0700

    update

--- js/3D4E.js
+++ js/3D4E.js
@@ -7,9 +7,15 @@ function initialize() {
         });
     }
     );
-    
-    $("#dummy").attr("min-height", $(".navbar").height());
-    
+
+    $("#dummy").height($(".navbar").height());
+    $("#calendar").attr("height", $("#calendar").width() * 9 / 16);
+
+    $(window).resize(function () {
+        $("#calendar").attr("width", "100%");
+        $("#calendar").attr("height", $("#calendar").width() * 9 / 16);
+    });
+
     var w = $(window).width() * .75;
     $('#my-slider').sliderPro({
         arrows: true,
