6dcf192e914868f8743acd93ff12e7799405da34
HyimHK <homingyim@gmail.com>
2015-07-13 20:21:54 -0700

    a

--- js/3D4E.js
+++ js/3D4E.js
@@ -2,7 +2,9 @@ function initialize() {
     $(function () {
         $("a.page-scroll").bind("click", function (e) {
             var t = $(this);
-            $("html, body").stop().animate({scrollTop: $(t.attr("href")).offset().top - $(".navbar").height()}, 750, "easeInOutExpo");
+            $("html, body").stop().animate(
+                    {scrollTop: $(t.attr("href")).offset().top - $(".navbar").height()}
+            , 750, "easeInOutExpo");
             e.preventDefault();
         });
     }
@@ -11,6 +13,17 @@ function initialize() {
     $("#dummy").height($(".navbar").height());
     $("#calendar").attr("height", $("#calendar").width() * 9 / 16);
 
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 800) {
+            $('#right-button').stop().animate({
+                right: "0"
+            }, 750,"easeInOutExpo");
+        } else {
+            $('#right-button').stop().animate({
+                right: "-50px"
+            }, 750,"easeInOutExpo");
+        }
+    });
     $(window).resize(function () {
         $("#calendar").attr("width", "100%");
         $("#calendar").attr("height", $("#calendar").width() * 9 / 16);
