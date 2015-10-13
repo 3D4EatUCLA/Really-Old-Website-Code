4f4f6471eb80fcf9b56720ba6e980723f5e935ab
Tiffany Wu <tiffanywu915@qq.com>
2015-10-13 00:12:47 -0700

    Revert "Revert "Fixed Gridster""
    This reverts commit 1f8a11c1cae7827e52c086691e3788f36e3e066c.

--- js/3D4E.js
+++ js/3D4E.js
@@ -1,25 +1,4 @@
-<<<<<<< HEAD
-function initialize() {
-    $(function () {
-        $("a.page-scroll").bind("click", function (e) {
-            var t = $(this);
-            $("html, body").animate(
-                    {scrollTop: $(t.attr("href")).offset().top - $(".navbar").height()}
-            , 750, "easeInOutQuad");
-            e.preventDefault();
-        });
-    }
-    );
-=======
  function initialize() {
-   loadLiquidFillGauge("fillgaugecomplete1", 100);
-   loadLiquidFillGauge("fillgaugecomplete2", 100);
-   loadLiquidFillGauge("fillgauge1", 15);
-   loadLiquidFillGauge("fillgauge2", 25);
-   loadLiquidFillGauge("fillgauge3", 15);
-   loadLiquidFillGauge("fillgauge4", 0);
-   
-
    $(function () {
      $("a.page-scroll").bind("click", function (e) {
        var t = $(this);
@@ -28,34 +7,71 @@ function initialize() {
        , 750, "easeInOutQuad");
        e.preventDefault();
      });
->>>>>>> homing-test
+   }
+   );
+   loadLiquidFillGauge("fillgaugecomplete1", 100);
+   loadLiquidFillGauge("fillgaugecomplete2", 100);
+   loadLiquidFillGauge("fillgauge1", 15);
+   loadLiquidFillGauge("fillgauge2", 25);
+   loadLiquidFillGauge("fillgauge3", 15);
+   loadLiquidFillGauge("fillgauge4", 0);
 
-    $("#dummy").height($(".navbar").height());
-    $("#calendar").attr("height", $("#calendar").width() * 9 / 16);
+   var gridster;
 
-    $(window).scroll(function () {
-        if ($(window).scrollTop() > 800) {
-            $('#right-button').stop().animate({
-                right: "0"
-            }, 750,"easeInOutExpo");
-        } else {
-            $('#right-button').stop().animate({
-                right: "-35px"
-            }, 750,"easeInOutExpo");
-        }
-    });
-    $(window).resize(function () {
-        $("#calendar").attr("width", "100%");
-        $("#calendar").attr("height", $("#calendar").width() * 9 / 16);
-    });
+   $(function () {
 
-    var w = $(window).width() * .75;
-    $('#my-slider').sliderPro({
-        arrows: true,
-        waitForLayers: true,
-        width: w,
-        height: 800,
-        fade: true,
-        buttons: false,
-    });
-}
+     gridster = $(".gridster ul").gridster({
+       widget_base_dimensions: [100, 100],
+       widget_margins: [5, 5],
+       helper: 'clone'
+     }).data('gridster');
+
+
+     // resize widgets on hover
+     gridster.$el
+      .on('mouseenter', '> li', function () {
+        gridster.resize_widget($(this), 5, 4);
+      })
+      .on('mouseleave', '> li', function () {
+        gridster.resize_widget($(this), 3, 1);
+      });
+
+   });
+   
+   $(function () {
+     $("a.page-scroll").bind("click", function (e) {
+       var t = $(this);
+       $("html, body").animate(
+        {scrollTop: $(t.attr("href")).offset().top - $(".navbar").height()}
+       , 750, "easeInOutQuad");
+       e.preventDefault();
+     });
+     $("#dummy").height($(".navbar").height());
+     $("#calendar").attr("height", $("#calendar").width() * 9 / 16);
+     $(window).scroll(function () {
+       if ($(window).scrollTop() > 800) {
+         $('#right-button').stop().animate({
+           right: "0"
+         }, 750, "easeInOutExpo");
+       } else {
+         $('#right-button').stop().animate({
+           right: "-35px"
+         }, 750, "easeInOutExpo");
+       }
+     });
+     $(window).resize(function () {
+       $("#calendar").attr("width", "100%");
+       $("#calendar").attr("height", $("#calendar").width() * 9 / 16);
+     });
+     var w = $(window).width() * .75;
+     $('#my-slider').sliderPro({
+       arrows: true,
+       waitForLayers: true,
+       width: w,
+       height: 800,
+       fade: true,
+       buttons: false
+     });
+   });
+ }
+ ;
