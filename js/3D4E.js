a866f454a88b9355e78fff4aede87cfe160cb76b
HyimHK <homingyim@gmail.com>
2015-11-12 20:21:59 -0800

    Remove SliderPro Code

--- js/3D4E.js
+++ js/3D4E.js
@@ -54,14 +54,5 @@
        $("#calendar").attr("width", "100%");
        $("#calendar").attr("height", $("#calendar").width() * 9 / 16);
      });
-     var w = $(window).width() * .75;
-     $('#my-slider').sliderPro({
-       arrows: true,
-       waitForLayers: true,
-       width: w,
-       height: 800,
-       fade: true,
-       buttons: false
-     });
    });
  };
